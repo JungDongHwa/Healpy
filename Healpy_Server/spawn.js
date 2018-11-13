3e725247c85b8138060cbbb32a3a8e2d88cb201e
swkim0128 <swkim0128@users.noreply.github.com>
2018-11-13 16:12:47 +0900

    server spawn revise
    서버 외부 exe 파일 실행 js 수정

--- Healpy_Server/spawn.js
+++ Healpy_Server/spawn.js
@@ -1,28 +1,32 @@
 'use strict';
 
-// const exec = require('child_process').execFile;
-// const cmd = 'node';
+const execFile = require('child_process').execFile;
+const cmd = "F:/Dev/OpenPose/openpose-1.4.0-win64-cpu-binaries/bin/OpenPoseDemo.exe";
  
-// exec(cmd, ['--version'], (error, stdout, stderr) => { 
-//     // command output is in stdout 
-//     if (error !== null) { 
-//         console.error('exec error:', error); 
-//         throw error;
-//     } 
-//     console.log('stdout:', stdout); 
-//     console.log('stderr:', stderr); 
-// });
+execFile(cmd, [], (error, stdout, stderr) => { 
+    // command output is in stdout 
+    if (error !== null) { 
+        console.error('exec error:', error); 
+        throw error;
+    } 
+    console.log('stdout:', stdout); 
+    console.log('stderr:', stderr); 
+});
 
-const spawn = require('child_process').spawn;
- 
-var child = spawn ( 'node' , [ '--version']);
+// const spawn = require('child_process').spawn;
 
-child.stdout.on ( 'data' , function (data) {  
-    console.log ( 'stdout :'  + data);  
-});  
-child.stderr.on ( 'data' , function (data) {  
-    console.log ( 'stderr :'  + data);  
-});  
-child.on ( 'close' , function (code) {  
-    console.log ( '자식 프로세스가 코드'  + code);  
-});  
+// var options = {
+//     cwd:'F:\Dev\OpenPose\openpose-1.4.0-win64-cpu-binaries'
+// };
+
+// var child = spawn ( 'bin\OpenPoseDemo.exe' , [], options);
+
+// child.stdout.on ( 'data' , function (data) {  
+//     console.log ( 'stdout :'  + data);  
+// });  
+// child.stderr.on ( 'data' , function (data) {  
+//     console.log ( 'stderr :'  + data);  
+// });  
+// child.on ( 'close' , function (code) {  
+//     console.log ( '자식 프로세스가 코드'  + code);  
+// });  
